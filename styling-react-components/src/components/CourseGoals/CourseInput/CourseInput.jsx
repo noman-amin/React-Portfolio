7272b8f6ebc422be97ccd5b0fac7c37410411a90
noman-amin <mail04noman@gmail.com>
2023-12-18 14:35:25 +0500

    Resolving styling issue arrived in CourseInput File

--- styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.jsx
+++ styling-react-components/src/components/CourseGoals/CourseInput/CourseInput.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import styles from "./CourseInput.module.css";
@@ -11,14 +10,14 @@ import styles from "./CourseInput.module.css";
 //     font-weight: bold;
 //     display: block;
 //     margin-bottom: 0.5rem;
-//     color: ${(props) => (props.invalid ? "red" : "black")};
+//     color: ${props => (props.invalid ? 'red' : 'black')};
 //   }
 
 //   & input {
 //     display: block;
 //     width: 100%;
-//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-//     background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+//     border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+//     background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
 //     font: inherit;
 //     line-height: 1.5rem;
 //     padding: 0 0.25rem;
@@ -53,9 +52,8 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
       <div
-        invalid={!isValid}
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
       >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
