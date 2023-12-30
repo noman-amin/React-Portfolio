58603a89381e2b955a2de349a4d1cf45e6e56958
noman-amin <mail04noman@gmail.com>
2023-12-30 12:16:12 +0500

    Using the useEffect Cleanup function to remove the excess timers

--- simple-loginForm-app/src/components/Login/Login.jsx
+++ simple-loginForm-app/src/components/Login/Login.jsx
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log("checking form validity!");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log("CLEAN UP");
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
