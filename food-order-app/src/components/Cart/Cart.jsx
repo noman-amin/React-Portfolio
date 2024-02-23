63a8bd22bc27fd84fe9fd02f78dbdac11d06f13d
noman-amin <mail04noman@gmail.com>
2024-02-23 12:02:19 +0500

    Outputting cart items & working on more comple Reducer

--- food-order-app/src/components/Cart/Cart.jsx
+++ food-order-app/src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -9,10 +10,20 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
