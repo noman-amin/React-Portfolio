49999451548ef1573d4e22624e6d2d85ea5c7519
noman-amin <mail04noman@gmail.com>
2024-07-30 20:38:13 +0500

    i Senging Get Request to Swapi Backend APi to request movies Data from their Database#

---
+++ connecting-to-database/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  function FetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transfomedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            release_date: movieData.release_date,
+          };
+        });
+        setMovies(transfomedMovies);
+      });
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={FetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
