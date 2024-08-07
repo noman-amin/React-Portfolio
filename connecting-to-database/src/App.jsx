00a334bb23368f08405acace21147ff3f6caaf7c
noman-amin <mail04noman@gmail.com>
2024-08-07 08:48:59 +0500

    Handling Loading & Data States

--- connecting-to-database/src/App.jsx
+++ connecting-to-database/src/App.jsx
@@ -5,7 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   async function FetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -18,6 +21,7 @@ function App() {
       };
     });
     setMovies(transfomedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -26,7 +30,9 @@ function App() {
         <button onClick={FetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length == 0 && <p>Found no Movies...</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
