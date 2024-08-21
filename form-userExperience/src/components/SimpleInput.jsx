f92d0303499555e7c230bc13d23721b94246e1fe
noman-amin <mail04noman@gmail.com>
2024-08-21 12:53:00 +0500

    gathering UserInput

--- form-userExperience/src/components/SimpleInput.jsx
+++ form-userExperience/src/components/SimpleInput.jsx
@@ -1,9 +1,35 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputRef = useRef();
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+    const setEnteredValue = nameInputRef.current.value;
+    console.log(setEnteredValue);
+
+    //nameInputRef.current.value = ""; //Not Ideal, Don't Manipulate the DOM
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          ref={nameInputRef}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
